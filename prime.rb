515b862bc4cd37535e188b11b5307281b7c385c8
Ana Harris <vana1990@gmail.com>
2018-09-28 18:48:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,14 +3,14 @@ def prime?(num)
   x = 2 
   result = true 
   while num > 0 
-  while x <= num/2 
-    if num % x == 0 
-      result = false 
-      break
-    end 
-  x += 1 
-  end
+    while x <= num/2 
+      if num % x == 0 
+        result = false 
+        break
+      end 
+    x += 1 
+    end
   return result
-end
+  end
 end
   
