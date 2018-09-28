b80ddd9951b0b819d07744e81249dcc9192bbb73
Ana Harris <vana1990@gmail.com>
2018-09-28 19:00:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,10 @@ def prime?(num)
   x = 2 
   result = true 
     while x <= num/2
+    if num <= 0 
+      result = false 
+      break
+    end
       if num % x == 0
         result = false 
         break
